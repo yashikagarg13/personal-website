41db6924623fb133ed5f04d3f17b4cf1f7ddc48e
Yashika Garg <yashikagarag13@gmail.com>
2016-12-24 17:21:26 +0530

    Step 4 - Build - Fix mobile view of cover image

--- src/components/cover-image.js
+++ src/components/cover-image.js
@@ -3,9 +3,11 @@ import React from "react";
 const CoverImage = () => {
   return (
     <div className="cover-image">
-      <div className="overlay">
-        <span className="sm text">Explore my skills and accomplishments</span>
-        <h2>Frontend Developer</h2>
+      <div className="container">
+        <div className="overlay">
+          <span className="sm text">Explore my skills and accomplishments</span>
+          <h2>Frontend Developer</h2>
+        </div>
       </div>
     </div>
   );
