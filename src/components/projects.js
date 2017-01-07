07dda8bdad73e782d525a6f13462351291863186
Yashika Garg <yashikagarag13@gmail.com>
2017-01-07 13:31:48 +0530

    Step 4 - Build - Projects in cards without media

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,68 +2,81 @@ import R from "ramda";
 import React from "react";
 
 import Carousel from "./common/carousel";
-import FormatHelpers from "../helpers/format";
 import ProjectsData from "../data/projects";
 
-/*
-*/
+export default class App extends React.Component {
+  constructor (props) {
+    super(props);
 
-const Projects = () => {
-  return (
-    <div id="projects" className="projects">
-      <div className="container text-center">
-        <i className="fa fa-product-hunt margin-right-xs lg primary text" aria-hidden="true"></i>
-        <span className="lg uppercase bold text">Projects</span>
-        {R.map(projectKey =>
-          <div key={`category-${FormatHelpers.strToCamelCase(projectKey)}`} className="box-wrapper">
-            <div className="headline">{projectKey}</div>
-            <div className="box">
-              <div className="project-wrapper">
-                <table>
-                  <tr>
-                    {R.map(project =>
-                      <td key={`project-${project.id}`} className="project">
-                        <div className="media-wrapper">
-                          <Carousel
-                            carouselId={`${FormatHelpers.strToCamelCase(project.title)}MediaCarousel`}
-                            data={project.media}
-                          />
-                        </div>
-                        <div className="padding-sm">
-                          <div className="clearfix">
-                            <div className="pull-left">
-                              {!R.isEmpty(project.url) && R.type(project.url) == "String"
-                                ? <a href={project.url} target="_blank" className="md bold text">{project.title}</a>
-                                : <span className="md bold text">{project.title}</span>}
-                            </div>
-                            <div className="pull-right">
-                              {!R.isEmpty(project.sourceCode) && R.type(project.sourceCode) == "String"
-                                ? <a href={project.sourceCode} target="_blank"><i className="fa fa-github accent text"></i></a>
-                                : null}
-                            </div>
-                          </div>
-                          {!R.isEmpty(project.client) && R.type(project.client) == "String"
-                            ? <div className="sm text">{project.client}</div>
-                            : null}
-                          <div className="xs dark-imp text margin-bottom-sm ">{project.type} <b>|</b> {project.role}</div>
-                          <div className="xs dark-vimp text margin-bottom-sm">{project.description}</div>
-                          <ul className="list">
-                            {R.map(technology =>
-                              <li key={`technology-${technology}`} className="list-item">{technology}</li>,
-                            project.technologies)}
-                          </ul>
-                        </div>
-                      </td>,
-                    ProjectsData[projectKey])}
-                  </tr>
-                </table>
-              </div>
-            </div>
-          </div>,
-        R.keys(ProjectsData))}
+    this.limit = 9;
+
+    this.state = {
+      limit: this.limit,
+    };
+
+    this.onClickShowAll = this.onClickShowAll.bind(this);
+    this.onClickShowLess = this.onClickShowLess.bind(this);
+  }
+
+  onClickShowAll () {
+    this.setState({
+      limit: R.length(ProjectsData),
+    });
+  }
+
+  onClickShowLess () {
+    this.setState({
+      limit: this.limit,
+    });
+  }
+
+  render () {
+    return (
+      <div id="projects" className="projects">
+        <div className="container text-center">
+          <i className="fa fa-product-hunt margin-right-xs lg primary text" aria-hidden="true"></i>
+          <span className="lg uppercase bold text">Projects</span>
+          <div className="box">
+            {R.map(project =>
+              <div key={`project-${project.id}`} className="project">
+                {/*<div className="media-wrapper">
+                  <Carousel
+                    carouselId={`${FormatHelpers.strToCamelCase(project.title)}MediaCarousel`}
+                    data={project.media}
+                  />
+                </div>*/}
+                <div className="padding-sm clearfix">
+                  <div className="clearfix">
+                    <div className="pull-left">
+                      {!R.isEmpty(project.url) && R.type(project.url) == "String"
+                        ? <a href={project.url} target="_blank" className="md bold text">{project.title}</a>
+                        : <span className="md bold text">{project.title}</span>}
+                    </div>
+                    <div className="pull-right">
+                      {!R.isEmpty(project.sourceCode) && R.type(project.sourceCode) == "String"
+                        ? <a href={project.sourceCode} target="_blank"><i className="fa fa-github accent text"></i></a>
+                        : null}
+                    </div>
+                  </div>
+                  {!R.isEmpty(project.client) && R.type(project.client) == "String"
+                    ? <div className="sm text">{project.client}</div>
+                    : null}
+                  <div className="xs dark-imp text margin-bottom-sm ">{project.type} <b>|</b> {project.role}</div>
+                  <div className="xs dark-vimp text margin-bottom-sm">{project.description}</div>
+                  <ul className="list">
+                    {R.map(technology =>
+                      <li key={`technology-${technology}`} className="list-item">{technology}</li>,
+                    project.technologies)}
+                  </ul>
+                </div>
+              </div>,
+            R.slice(0, this.state.limit, ProjectsData))}
+          </div>
+          <button className="btn btn-primary show-all-btn"
+            onClick={this.state.limit < R.length(ProjectsData) ? this.onClickShowAll : this.onClickShowLess}>
+            {this.state.limit < R.length(ProjectsData) ? "Show All" : "Show Less"}</button>
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default Projects;
+    );
+  }
+}
